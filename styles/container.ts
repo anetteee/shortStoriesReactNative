a20f4dabb05b18037bb0662d8e72d02a402240d4
CamillaKK <camillakveimkopperud@gmail.com>
2022-11-08 11:41:51 +0100

    #3 Add container styling

---
+++ styles/container.ts
@@ -0,0 +1,19 @@
+import { Colors } from ".";
+import { Dimensions, StyleSheet } from "react-native";
+
+var width = Dimensions.get("window").width; //full width
+//var height = Dimensions.get("window").height; //full height
+
+const containerStyles = StyleSheet.create({
+    scrollContainer: {
+      flex: 1,
+      width: width,
+    },
+    parentContainer: {
+      width: "100%",
+      padding: 20,
+      margin: 5,
+    },
+  });
+
+export default containerStyles;
