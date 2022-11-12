e68c7933f0e9312e1c3503fc5ecf7856cd0ca968
CamillaKK <camillakveimkopperud@gmail.com>
2022-11-12 12:40:39 +0100

    #3 Add safeArea to MainPage

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { SafeAreaView } from "react-native";
 import Results from "./Results";
 import SearchSection from "./SearchSection";
 
 export default function MainPage() {
   return (
-    <>
+    <SafeAreaView style={{ flex: 1 }}>
       <SearchSection />
       <Results />
-    </>
+    </SafeAreaView>
   );
 }
