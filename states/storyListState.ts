d6e08495a2da01fb16a1a42f874ba3f71e1abe18
CamillaKK <camillakveimkopperud@gmail.com>
2022-11-14 11:46:43 +0100

    #10 Add useState to Story instead of recoil

--- states/storyListState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-
-//The atom represents the state
-export const expandedStoriesListState = atom<String[]>({
-    key: "storyState", //Unique identifier internally
-    default: [], //Initial value of atom
-});
