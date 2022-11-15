77b3cbc4391aaca3b3c266f8a8f15eab316fc987
rastrand <rastrand@stud.ntnu.no>
2022-11-15 09:16:36 +0100

    #15 Add linear gradient background color

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
 import { ApolloProvider } from "@apollo/client";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Dimensions, Text, View, SafeAreaView } from "react-native";
 import { apolloClient } from "./apolloClient";
 import MainPage from "./components/MainPage";
 import { RecoilRoot } from "recoil";
+import { theme } from "./styles/theme";
+import { LinearGradient } from "expo-linear-gradient";
+
+var width = Dimensions.get("window").width; //full width
+var height = Dimensions.get("window").height; //full height
 
 export default function App() {
   return (
     <RecoilRoot>
       <ApolloProvider client={apolloClient}>
         <View style={styles.container}>
-          <MainPage />
+          <LinearGradient
+            colors={[
+              theme.colors.bluish,
+              theme.colors.darkGreen,
+              // theme.colors.lightGreen,
+            ]}
+            style={{
+              width: width,
+              height: height,
+            }}
+            start={[0, 0]}
+            end={[1, 0]}
+          >
+            <SafeAreaView>
+              <MainPage />
+            </SafeAreaView>
+          </LinearGradient>
         </View>
       </ApolloProvider>
     </RecoilRoot>
@@ -20,8 +41,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: theme.colors.bluish,
     alignItems: "center",
     justifyContent: "center",
+  },
+  linearGradient: {
+    width: 350,
   },
 });
