41a395b0395559dbbfc4842173e4b0e3e67943d9
rastrand <rastrand@stud.ntnu.no>
2022-11-15 09:19:32 +0100

    #15 Add header section

--- components/SearchSection.tsx
+++ components/SearchSection.tsx
@@ -3,14 +3,21 @@ import { StatusBar, View, Text, StyleSheet } from "react-native";
 import SearchInput from "./SearchInput";
 import SelectFilter from "./selectFilter";
 import SelectSort from "./selectSort";
+import { text } from "../styles/theme";
+import { wrappers } from "../styles/containers";
+import { containers } from "../styles/containers";
 
 export default function SearchSection() {
   return (
     <View>
+      <View style={containers.headerContainer}>
+        <Text style={text.h1}>Fantastic short stories</Text>
+        <Text style={text.h2}>Search among hundreds of titles</Text>
+      </View>
       <SearchInput />
       <SelectFilter />
-      <SelectSort />
-      <StatusBar />
+      {/* <SelectSort /> */}
+      {/* <StatusBar /> */}
     </View>
   );
 }
