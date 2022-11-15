da101224dba1e3e6fe6af6a6b9197719b907f25a
rastrand <rastrand@stud.ntnu.no>
2022-11-15 09:21:50 +0100

    #15 Add margin+padding around search and filter elements

--- styles/containers.ts
+++ styles/containers.ts
@@ -4,7 +4,30 @@ import { Dimensions, StyleSheet } from "react-native";
 var width = Dimensions.get("window").width; //full width
 //var height = Dimensions.get("window").height; //full height
 
-const containerStyles = StyleSheet.create({
+export const containers = StyleSheet.create({
+    headerContainer: {
+      paddingTop:10,
+      minHeight: 120,
+      textAlign: "center",
+      alignContent: "center"
+    },
+  
+    searchContainer: {
+      minHeight: 120,
+    },
+    filterContainer: {
+      marginTop: 30,
+      paddingTop:10,
+      minHeight: 120,
+    },
+    sortContainer: {
+      marginTop: 30,
+      paddingTop:10,
+      minHeight: 120,
+    },
+   
+  
+
     scrollContainer: {
       flex: 1,
       width: width,
@@ -16,4 +39,18 @@ const containerStyles = StyleSheet.create({
     },
   });
 
-export default containerStyles;
+export const wrappers = StyleSheet.create({
+  yellow: {
+    borderWidth: 1, 
+    borderColor: "yellow"
+  }, 
+  purple: {
+    borderWidth: 1, 
+    borderColor: "purple"
+  }, 
+  pink: {
+    borderWidth: 1, 
+    borderColor: "#FFB6C1"
+
+  }
+});
